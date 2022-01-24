42f1641a722d6567abb8a9fdbc6ad3e1754bdcce
yeah1832 <aer561@naver.com>
2022-01-24 12:58:26 +0900

    Add files via upload

---
+++ Creature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    public enum CreatureType
+    { 
+        None,
+        Player = 1,
+        Monster = 2
+    }
+    class Creature
+    {
+        CreatureType type;
+        protected Creature(CreatureType type) {
+            this.type = type;
+        }
+        protected int hp = 0;
+        protected int attack = 0;
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool isDead() { return hp <= 0; }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+    }
+}
